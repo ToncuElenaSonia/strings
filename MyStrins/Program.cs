9d1e3efe2fae81d4ca96f4938266972b8a22ca9f
ToncuElenaSonia <sonya_helena@yahoo.com>
2019-05-18 12:29:41 +0300

    upload 2

---
+++ MyStrins/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyStrins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Hello World!";
+            for (int i = 0; i < myString.Length; i++)
+            {
+                Console.WriteLine(myString[i]);
+      
+             }
+            for (int i = 0; i < myString.Length; i++)
+            {
+                Console.Write(myString[i]);
+            }
+            string myString1 = "My awsome string";
+            Console.WriteLine(myString1.Length);
+
+            string s=string.Empty;
+            string b = s;
+            Console.WriteLine(s);
+            Console.WriteLine($"b: {b}");
+
+            int myInt = 30;
+            string myIntAsString = myInt.ToString();
+            Console.WriteLine($"myIntAsString: {myIntAsString}");
+
+        }
+    }
+}
